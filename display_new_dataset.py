dd8de76af6da5178b955bd6b8a32666b6e991fe7
xinwei39 <37181792+xinwei39@users.noreply.github.com>
2021-11-12 22:31:29 +0800

    Add files via upload

--- display_new_dataset.py
+++ display_new_dataset.py
@@ -3,14 +3,23 @@ import configparser
 config = configparser.ConfigParser()
 config.read('configuration.ini')
 
+#read in the dataset file
 file = config['display_new_dataset']['dataset_file']
 
-file = 'tweets_dataset_textblob.csv'
-df = pd.read_csv(file, index_col=None, lineterminator='\n', header=0, names= ['Datetime','Clean_Text','Text','Subjectivity','Polarity','Sentiment'], dtype='unicode')
+df = pd.read_csv(file, index_col=None, lineterminator='\n', header=None, names= ['Datetime','Clean_Text','Text','Subjectivity','Polarity','Sentiment'], dtype='unicode')
+df = df.astype( dtype={
+                'Datetime' : str,
+                'Clean_Text': str,
+                'Text': str,
+                'Subjectivity': float,
+                'Polarity': float,
+                'Sentiment': str})
 
-print("The first 5 records")
-print(df.head(5))
-print("The last 5 records")
-print(df.tail(5))
+
+print("The first 1 record")
+print(df.head(1))
+print("The last 1 record")
+print(df.tail(1))
 print("total number of records:")
 print(len(df))
+
